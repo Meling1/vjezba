f6160eb792d231c85c94775a989fbc32d9ee8c6c
Meling1 <120015690+Meling1@users.noreply.github.com>
2024-05-24 15:13:31 +0200

    dodat addEventListeners

---
+++ zadatak.js
@@ -0,0 +1,54 @@
+/* napraviti html stranicu koja ima 2 input boxa u koje mozemo unijeti samo brojeve.
+Dodati na stranicu treci input box koji je readonly u koji ce se ispisati rezultat izracunavanja.
+dodati na stranicu button koji ce na onclick event pozvati funkciju iz javascripta
+dodatak za vecu ocjenu:
+- dodati button koji ce random unijeti dva broja u input boxove i odraditi automatsko izracunavanje
+
+napraviti javascript sa funkcijom koja ce raditi sljedece:
+- za parametre prima 2 ulazna podatka
+- provjeriti da su ulazni podaci brojevi
+- u slucaju da je jedan od podataka krivi, odnosno nije broj, u label ispisati gresku, odnosno rezultat funkcije
+(funkcija mora vratiti tocnu gresku)
+- u slucaju da su ulazni parametri ispravni odraditi sljedece:
+- vratiti rezultat koji je od dva ulazna broja veci
+- ukoliko su oba broja jednaka, vratiti prvi
+
+- css-om odraditi sljedece:
+- ukoliko je funkcija vratila ispravnu vrijednost obojati pozadinu treceg textbox-a u zelenu boju
+- ukoliko je funkcija vratila gresku obojati pozadinu treceg textboxa-a u crvenu boju
+*/
+
+function ime1(a, b) {
+    if (typeof a !== "number") {
+      return "Greška: prvi parametar nije broj";
+    }
+    if (typeof b !== "number") {
+      return "Greška: drugi parametar nije broj";
+    }
+    if (a > b) {
+      return a;
+    }
+    if (a < b) {
+      return b;
+    }
+    if (a === b) {
+      return a;
+    }
+  }
+
+var input = document.getElementById("unos");
+var inputVal1 = input.value;
+
+var input1 = document.getElementById("unos1");
+var inputVal2 = input1.value;
+
+var textBox =  document.getElementById("input");
+
+ function zbroj() {
+    return inputVal1 + inputVal2; 
+  }
+
+  function click(){
+  textBox.innerHTML(zbroj());
+  }
+
